6c0d5e2d1fac572f50a95e3247ef73986175ff80
ADZL18 <zazuetaangel41@gmail.com>
2024-04-10 04:42:30 -0700

    terminé la tarea

--- diccionarios.py
+++ diccionarios.py
@@ -1,3 +1,12 @@
-a= 1
-b= 2
-c= a+b
+libro_calificaciones_matematicas= {}
+libro_calificaciones_matematicas["kevin"]= "7"
+libro_calificaciones_matematicas["Demetrio"]= "9"
+libro_calificaciones_matematicas["Sergio"]= "6"
+libro_calificaciones_matematicas["Uriel"]= "7"
+libro_calificaciones_matematicas["Emily"]= "10"
+libro_calificaciones_matematicas["Damary"]= "7"
+libro_calificaciones_matematicas["Vianey"]= "10"
+libro_calificaciones_matematicas["Alain"]= "10"
+libro_calificaciones_matematicas["juan"]= "9"
+libro_calificaciones_matematicas["carlos"]= "8"
+print ("la calificacion kevin en matematicas es:",libro_calificaciones_matematicas["kevin"])
